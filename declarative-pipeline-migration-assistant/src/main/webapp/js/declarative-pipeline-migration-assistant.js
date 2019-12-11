f51464415e01d2ec86184bb58b9651c33ed0aac4
olivier lamy <olamy@apache.org>
2019-12-11 10:48:42 +1000

    use prism to display Jenkinsfile
    Signed-off-by: olivier lamy <olamy@apache.org>

--- declarative-pipeline-migration-assistant/src/main/webapp/js/declarative-pipeline-migration-assistant.js
+++ declarative-pipeline-migration-assistant/src/main/webapp/js/declarative-pipeline-migration-assistant.js
@@ -1,1 +1,14 @@
+Prism.plugins.toolbar.registerButton('download-jenkinsfile', {
+  text: 'Download', // required
+  onClick: function (env) { // optional
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(env.code));
+    element.setAttribute('download', "Jenkinsfile");
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }
+});
 
+// $("#jenkinsfile-content").hide();
