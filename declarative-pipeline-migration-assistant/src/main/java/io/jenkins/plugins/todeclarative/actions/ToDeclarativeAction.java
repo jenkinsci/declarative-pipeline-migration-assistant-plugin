28eebb20ca1124b159d59f68b46d685ad692b4f5
Carroll Chiou <cchiou@cloudbees.com>
2020-04-01 21:29:33 -0700

    remove listener code

--- declarative-pipeline-migration-assistant/src/main/java/io/jenkins/plugins/todeclarative/actions/ToDeclarativeAction.java
+++ declarative-pipeline-migration-assistant/src/main/java/io/jenkins/plugins/todeclarative/actions/ToDeclarativeAction.java
@@ -10,7 +10,6 @@ import io.jenkins.plugins.todeclarative.converter.api.ConverterRequest;
 import io.jenkins.plugins.todeclarative.converter.api.ConverterResult;
 import io.jenkins.plugins.todeclarative.converter.api.Warning;
 import io.jenkins.plugins.todeclarative.converter.freestyle.FreestyleToDeclarativeConverter;
-import io.jenkins.plugins.todeclarative.listener.DeclarativeConverterListener;
 import jenkins.model.Jenkins;
 import jenkins.model.TransientActionFactory;
 import org.jenkinsci.plugins.pipeline.modeldefinition.ast.ModelASTPipelineDef;
@@ -54,7 +53,6 @@ public class ToDeclarativeAction
             this.jenkinsFile = converterResult.getModelASTPipelineDef().toPrettyGroovy();
             this.warnings = converterResult.getWarnings();
             //this.warnings.add( new Warning( "Not supported plugin", "Awesome plugin class" ) );
-            DeclarativeConverterListener.fire(job, converterResult);
             return jenkinsFile;
         } catch ( Exception e )
         {
