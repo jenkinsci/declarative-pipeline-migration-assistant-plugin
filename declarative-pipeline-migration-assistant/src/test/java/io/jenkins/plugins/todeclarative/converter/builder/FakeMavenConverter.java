ecca3bd76c1f071ad0d7287c130507c07506b336
Carroll Chiou <cchiou@cloudbees.com>
2020-04-03 14:18:06 -0700

    remove old interfaces from api update implementation of fake maven converter

--- declarative-pipeline-migration-assistant/src/test/java/io/jenkins/plugins/todeclarative/converter/builder/FakeMavenConverter.java
+++ declarative-pipeline-migration-assistant/src/test/java/io/jenkins/plugins/todeclarative/converter/builder/FakeMavenConverter.java
@@ -1,48 +1,23 @@
 package io.jenkins.plugins.todeclarative.converter.builder;
 
 import hudson.Extension;
-import hudson.model.FreeStyleProject;
-import hudson.model.JDK;
 import hudson.tasks.Builder;
 import hudson.tasks.Maven;
-import hudson.util.ArgumentListBuilder;
+import io.jenkins.plugins.todeclarative.converter.api.ConverterException;
 import io.jenkins.plugins.todeclarative.converter.api.ConverterRequest;
 import io.jenkins.plugins.todeclarative.converter.api.ConverterResult;
-import io.jenkins.plugins.todeclarative.converter.api.builder.BuilderConverter;
-import jenkins.mvn.FilePathGlobalSettingsProvider;
-import jenkins.mvn.FilePathSettingsProvider;
-import jenkins.mvn.GlobalSettingsProvider;
-import jenkins.mvn.SettingsProvider;
-import org.apache.commons.lang.StringUtils;
-import org.jenkinsci.plugins.pipeline.modeldefinition.ast.ModelASTBranch;
-import org.jenkinsci.plugins.pipeline.modeldefinition.ast.ModelASTEnvironment;
-import org.jenkinsci.plugins.pipeline.modeldefinition.ast.ModelASTEnvironmentValue;
-import org.jenkinsci.plugins.pipeline.modeldefinition.ast.ModelASTKey;
-import org.jenkinsci.plugins.pipeline.modeldefinition.ast.ModelASTSingleArgument;
+import io.jenkins.plugins.todeclarative.converter.api.SingleTypedConverter;
 import org.jenkinsci.plugins.pipeline.modeldefinition.ast.ModelASTStage;
-import org.jenkinsci.plugins.pipeline.modeldefinition.ast.ModelASTStep;
-import org.jenkinsci.plugins.pipeline.modeldefinition.ast.ModelASTTools;
-import org.jenkinsci.plugins.pipeline.modeldefinition.ast.ModelASTValue;
-
-import java.util.Arrays;
-import java.util.Map;
 
 @Extension( ordinal = 10 )
 /**
  * we just want to make sure the ordinal ordering works and we pick the smaller one and not this one
  */
-public class FakeMavenConverter
-    implements BuilderConverter
+public class FakeMavenConverter extends SingleTypedConverter<Maven>
 {
     @Override
-    public ModelASTStage convert( ConverterRequest request, ConverterResult converterResult, Builder builder )
+    public boolean convert(ConverterRequest request, ConverterResult result, Object target) throws ConverterException
     {
-        return null;
-    }
-
-    @Override
-    public boolean canConvert( Builder builder )
-    {
-        return builder instanceof Maven;
+        return true;
     }
 }
