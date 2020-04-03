2895a3dafa9eddb3c3ae27671892ffd16f7f2a7c
Carroll Chiou <cchiou@cloudbees.com>
2020-04-03 15:32:29 -0700

    Remove unnecessary null check, already occurs in helper method

--- declarative-pipeline-migration-assistant/src/main/java/io/jenkins/plugins/todeclarative/converter/publisher/JUnitResultArchiverConverter.java
+++ declarative-pipeline-migration-assistant/src/main/java/io/jenkins/plugins/todeclarative/converter/publisher/JUnitResultArchiverConverter.java
@@ -5,7 +5,6 @@ import io.jenkins.plugins.todeclarative.converter.api.ConverterRequest;
 import io.jenkins.plugins.todeclarative.converter.api.ConverterResult;
 import io.jenkins.plugins.todeclarative.converter.api.ModelASTUtils;
 import io.jenkins.plugins.todeclarative.converter.api.SingleTypedConverter;
-import org.jenkinsci.plugins.pipeline.modeldefinition.ast.ModelASTBranch;
 import org.jenkinsci.plugins.pipeline.modeldefinition.ast.ModelASTBuildCondition;
 import org.jenkinsci.plugins.pipeline.modeldefinition.ast.ModelASTStep;
 import org.jenkinsci.plugins.variant.OptionalExtension;
@@ -20,11 +19,6 @@ public class JUnitResultArchiverConverter extends SingleTypedConverter<JUnitResu
         ModelASTBuildCondition buildCondition =
             ModelASTUtils.buildOrFindBuildCondition( result.getModelASTPipelineDef(), "always" );
 
-        ModelASTBranch branch = buildCondition.getBranch();
-        if(branch==null){
-            branch =new ModelASTBranch( this );
-            buildCondition.setBranch( branch );
-        }
         // junit(testResults: 'foof', allowEmptyResults: true, healthScaleFactor: 1.0,
         // keepLongStdio: true)
         ModelASTStep junit =  ModelASTUtils.buildGenericStep( jUnitResultArchiver, this );
