af391b8e42c9246a4317850e5def96eb6aa462d6
Olivier Lamy <olamy@apache.org>
2023-06-19 18:22:50 +1000

    Add new field create project to converter request
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- declarative-pipeline-migration-assistant-api/src/main/java/io/jenkins/plugins/todeclarative/converter/api/ConverterRequest.java
+++ declarative-pipeline-migration-assistant-api/src/main/java/io/jenkins/plugins/todeclarative/converter/api/ConverterRequest.java
@@ -12,6 +12,8 @@ public class ConverterRequest
 {
     private Job job;
 
+    private boolean createNewProject;
+
     /**
      * Map to store some values related to the current conversion
      * if you use that please add the key name as a constant
@@ -37,6 +39,19 @@ public class ConverterRequest
     public ConverterRequest job( Job job )
     {
         this.job = job;
+        return this;
+    }
+
+    public boolean isCreateNewProject() {
+        return createNewProject;
+    }
+
+    public void setCreateNewProject(boolean createNewProject) {
+        this.createNewProject = createNewProject;
+    }
+
+    public ConverterRequest createNewProject(boolean createNewProject) {
+        this.createNewProject = createNewProject;
         return this;
     }
 
